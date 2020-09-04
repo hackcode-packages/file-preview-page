49406d21ed89df753af90074864f6866d9334928
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-09-04 14:38:51 +0530

    code: filetype tags

--- src/auxillary.ts
+++ src/auxillary.ts
@@ -2,6 +2,8 @@ import * as chalk from "chalk";
 import * as util from "util";
 import * as fs from "fs";
 
+import { FileMetaData } from "./interface";
+
 /**
  * @description Method to display clean logs in terminal
  * @param type string the category to which statement belongs
@@ -32,3 +34,34 @@ export const log = (type: string, desc: string, level: number = 0): void => {
 /** using system calls with async/await */
 export const lstat = util.promisify(fs.lstat);
 export const readdir = util.promisify(fs.readdir);
+
+/**
+ * function to get file extension and type from name
+ * @param fileName string filename to extract extension and filetype from
+ */
+export const getFileType = (fileName: string): FileMetaData => {
+  const images = ["png", "jpg", "gif", "img", "jpeg"];
+  const documents = ["docx", "doc", "pdf", "xls", "xlsx", "md"];
+  const compressed = ["zip", "tar", "gz", "iso", "rar", "7z"];
+  const code = ["js", "json", "cpp", "java", "class", "ts", "php"];
+
+  const extension = fileName.split(".").pop();
+  let fileType = undefined;
+
+  if (images.includes(extension)) {
+    fileType = "image";
+  } else if (documents.includes(extension)) {
+    fileType = "document";
+  } else if (compressed.includes(extension)) {
+    fileType = "compressed";
+  } else if (code.includes(extension)) {
+    fileType = "code";
+  } else {
+    fileType = "file";
+  }
+
+  return {
+    extension,
+    type: fileType,
+  };
+};
--- src/interface.ts
+++ src/interface.ts
@@ -6,4 +6,10 @@ export interface FileObject {
   name: String;
   type: "leaf" | "node";
   children: Array<FileObject> | null;
+  meta?: FileMetaData;
+}
+
+export interface FileMetaData {
+  extension: string;
+  type: "image" | "compressed" | "document" | undefined;
 }
